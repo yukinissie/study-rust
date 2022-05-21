9e78e3dc1d79b08c84d953fe3014cb912c78fb7c
yukinissie <bigtree2490@outlook.jp>
2022-05-21 11:46:00 +0900

    Rustの特徴

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@ fn main() {
     let question = "The Question".to_string();
     let answer = 43;
     println!("Question: {}\nAnswer: {}", question, answer);
+
+    let a; // Rustは後のコードも評価して型推論をする。
+    let b; // Kotlinだと型推論できずコンパイルエラーになる。
+    a = 3;
+    b = 4;
+    println!("{}", max(a, b));
 }
 
-// fn max(a: i32, b: i32) -> i32 {
-//     return if a > b { a } else { b }
-// }
+fn max(a: i32, b: i32) -> i32 {
+    return if a > b { a } else { b }
+}
