64d09346f6328cc5b44f4c01c2648a7e773a023d
yukinissie <bigtree2490@outlook.jp>
2022-05-21 11:37:26 +0900

    変数お試し

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    println!("{}", max(1, 10));
+    let question = "The Question".to_string();
+    let answer = 43;
+    println!("Question: {}\nAnswer: {}", question, answer);
 }
 
-fn max(a: i32, b: i32) -> i32 {
-    return if a > b { a } else { b }
-}
+// fn max(a: i32, b: i32) -> i32 {
+//     return if a > b { a } else { b }
+// }
